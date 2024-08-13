3d002076e5f7954a010680b46ec49348eb5b63f5
AhmedHassan <Ahmedhassan@fci.helwan.edu.eg>
2024-08-13 06:16:34 +0300

    create a pojo class Meal

---
+++ app/src/main/java/com/example/foodplanneritiandroidjava/network/PojoClasses/Meal.java
@@ -0,0 +1,37 @@
+package com.example.foodplanneritiandroidjava.network.PojoClasses;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Meal {
+    @SerializedName("idMeal")
+    private String id;
+    @SerializedName("strMeal")
+    private String name;
+    @SerializedName("strMealThumb")
+    private String thumbnail;
+    // getters and setters
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
