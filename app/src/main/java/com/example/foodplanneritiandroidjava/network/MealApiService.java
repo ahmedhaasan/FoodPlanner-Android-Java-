f4a6028626988be16d2c7b9cb085a640fad02a03
AhmedHassan <Ahmedhassan@fci.helwan.edu.eg>
2024-08-13 06:14:34 +0300

    creating a Pojo Classes package  with frist Class Category

---
+++ app/src/main/java/com/example/foodplanneritiandroidjava/network/MealApiService.java
@@ -0,0 +1,10 @@
+package com.example.foodplanneritiandroidjava.network;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+/*public interface MealApiService {
+    @GET("categories.php")
+
+
+}*/
