122966b64245ec040c82608a1d6b51a076fd324f
AhmedHassan <Ahmedhassan@fci.helwan.edu.eg>
2024-08-15 15:18:04 +0300

    creating and validating signIn with fire Base authentication

--- app/src/main/java/com/example/foodplanneritiandroidjava/login_fragment.java
+++ app/src/main/java/com/example/foodplanneritiandroidjava/login_fragment.java
@@ -1,58 +1,69 @@
 package com.example.foodplanneritiandroidjava;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link login_fragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+
 public class login_fragment extends Fragment {
+    EditText registerText ;
+    View v1;
+    MaterialButton sign_in_button;
+    SignInButton signWithGoogleButton;
+    TextInputLayout email_layout, password_layout;
+    TextInputEditText email_edit_text, password_editText;
+    Dialog loadingDialog;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    //
+    FirebaseAuth authenticate;
+    FirebaseDatabase database;
+    GoogleSignInClient googleSignInClient;
+    int RC_SIGN_IN = 30;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public login_fragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment login_fragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static login_fragment newInstance(String param1, String param2) {
-        login_fragment fragment = new login_fragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        authenticate = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        GoogleSignInOptions gson = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail().build();
+        googleSignInClient = GoogleSignIn.getClient(getContext(), gson);
     }
 
     @Override
@@ -60,5 +71,89 @@ public class login_fragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        v1 = view;
+        registerText = view.findViewById(R.id.registerText);
+        sign_in_button = view.findViewById(R.id.signInButton);
+        signWithGoogleButton = view.findViewById(R.id.signInWithGoogleButton);
+        email_layout = view.findViewById(R.id.emailInputLayout);
+        password_layout = view.findViewById(R.id.passwordInputLayout);
+        email_edit_text = view.findViewById(R.id.emailEditText);
+        password_editText = view.findViewById(R.id.passwordEditText);
+        loadingDialog = new Dialog(getContext());
+        loadingDialog.setContentView(R.layout.loading_dialog);
+        loadingDialog.setCancelable(false);
+        loadingDialog.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        // handel signIn with google
+        signWithGoogleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // navigate to home activity
+                signIn();
+
+            }
+
+
+        });
+
+        // handle Not have account Register
+        registerText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(v1).navigate(R.id.action_login_fragment_to_signUp_fragment);
+
+            }
+        });
+    }
+
+    private void signIn() {
+        Intent intent = googleSignInClient.getSignInIntent();
+        startActivityForResult(intent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+        try {
+            GoogleSignInAccount account = task.getResult(ApiException.class);
+            firebaseAuth(account.getIdToken());
+
+        } catch (ApiException e) {
+            Toast.makeText(getContext(), e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void firebaseAuth(String idToken) {
+
+        loadingDialog.show(); // show the progress par
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        authenticate.signInWithCredential(credential)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Toast.makeText(getContext(), "Account Cteated Successfully", Toast.LENGTH_SHORT).show();
+                        FirebaseUser user = authenticate.getCurrentUser();
+                        HashMap<String, Object> map = new HashMap<>();
+                        map.put("name", user.getDisplayName());
+                        map.put("email", user.getEmail());
+                        map.put("profile", user.getPhotoUrl().toString());
+                        database.getReference().child("googleAuth").child(user.getUid())
+                                .setValue(map).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void unused) {
+                                        loadingDialog.dismiss(); // hide the progress par
+                                        Navigation.findNavController(v1).navigate(R.id.action_login_fragment_to_homeActivity);
+
+                                    }
+                                });
+                    }
+                });
     }
 }
